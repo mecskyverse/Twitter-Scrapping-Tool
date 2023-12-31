2d53f86e9df2d35474f405634a44434ce7a1bb73
Aakash <vermaaakash1000@gmail.com>
2023-12-31 21:56:51 +0530

    openai API integration added

--- tweets.py
+++ tweets.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import config
 from ntscraper import Nitter
+import openai
 # query = "python"
 
-scaper = Nitter()
-tweets = scaper.get_tweets('mecskyverse', mode='user', number=-1, filters=["replies"])
-print('pages are ',tweets)
+print(config.API_KEY)
+
+# scaper = Nitter()
+# tweets = scaper.get_tweets('mecskyverse', mode='user', number=-1, filters=["replies"])
+# print('pages are ',tweets)
